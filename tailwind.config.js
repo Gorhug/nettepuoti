28ce71710e385f22def328b682496739a3dbb034
Ilkka Forsblom <quiverth@gmail.com>
2023-12-19 03:53:45 +0200

    teema kuntoon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,22 @@
 module.exports = {
-    content: ['./app/**/Presenters/**/*.{latte,js,ts,php,svg}', ],
-    plugins: [require('daisyui')],
-  };
-  
+  content: ['./app/**/Presenters/**/*.{latte,js,ts,php,svg}',],
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        gorhug: {
+          ...require("daisyui/src/theming/themes")["business"],
+          "primary": "#ff8000",
+          "secondary": "#800080",
+          "accent": "#8000ff",
+          "neutral": "#0f0a09",
+          "base-100": "#270510",
+          "info": "#00d6f9",
+          "success": "#008040",
+          "warning": "#ffff00",
+          "error": "#ff0000",
+        },
+      },
+    ],
+  },
+};
