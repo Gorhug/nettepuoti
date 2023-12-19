84e1f75a66dbf37e4f2d03d70e3c837c590b38ae
Ilkka Forsblom <quiverth@gmail.com>
2023-12-19 23:35:35 +0200

    parsedown lisätty

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   content: ['./app/**/Presenters/**/*.{latte,js,ts,php,svg}',],
-  plugins: [require('daisyui')],
+  plugins: [require("@tailwindcss/typography"), require('daisyui')],
   daisyui: {
     themes: [
       {
