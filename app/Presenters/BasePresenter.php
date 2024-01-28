e8c9578cb6346169b425ac6b3850c67fbf9a4cc8
Ilkka Forsblom <quiverth@gmail.com>
2024-01-28 02:02:04 +0200

    aloitettu opengraph, päivämäärä spotteihin

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -20,17 +20,31 @@ abstract class BasePresenter extends Presenter
         $this->redrawControl('content');
         $this->template->locale = $this->locale;
         $fullLoc = $this->locale === 'fi' ? 'fi_FI' : 'en_FI';
-        $dater = new IntlDateFormatter($fullLoc,
+        $dater = new IntlDateFormatter(
+            $fullLoc,
             IntlDateFormatter::RELATIVE_FULL,
             IntlDateFormatter::NONE,
             'Europe/Helsinki',
-            IntlDateFormatter::GREGORIAN);
+            IntlDateFormatter::GREGORIAN
+        );
         $this->template->addFilter('intlDay', fn($date) => $dater->format($date));
-        $hourer = new IntlDateFormatter($fullLoc,
-        IntlDateFormatter::FULL,
-        IntlDateFormatter::FULL,
-        'Europe/Helsinki',
-        IntlDateFormatter::GREGORIAN, 'HH');
+        $hourer = new IntlDateFormatter(
+            $fullLoc,
+            IntlDateFormatter::FULL,
+            IntlDateFormatter::FULL,
+            'Europe/Helsinki',
+            IntlDateFormatter::GREGORIAN,
+            'HH'
+        );
         $this->template->addFilter('intlHour', fn($date) => $hourer->format($date));
+
+        $fullDater = new IntlDateFormatter(
+            $fullLoc,
+            IntlDateFormatter::FULL,
+            IntlDateFormatter::NONE,
+            'Europe/Helsinki',
+            IntlDateFormatter::GREGORIAN
+        );
+        $this->template->addFilter('intlFullDay', fn($date) => $fullDater->format($date));
     }
 }
